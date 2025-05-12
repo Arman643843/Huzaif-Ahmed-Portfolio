f1328454d9918920ad04d6e55f3038529b6d9bca
Arman643843 <siyam859560@gmail.com>
2025-05-12 23:25:11 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { FaInstagram, FaXTwitter, FaLinkedin } from "react-icons/fa6";
+import { FaInstagram, FaXFacebook, FaLinkedin } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
       <a
-        href="https://www.instagram.com/huzaifahmedz/"
+        href="https://https://www.instagram.com/_arxman._/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaInstagram size={24} />
       </a>
-      <a href="https://twitter.comhttps://x.com/HuzaifDev" target="_blank" rel="noopener noreferrer">
-        <FaXTwitter size={24} />
+      <a href="https://www.facebook.com/share/1HecLnQbMA/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer">
+        <FaXFacebook size={24} />
       </a>
       <a
-        href="https://www.linkedin.com/in/huzaif-ahmed-6abb49273/"
+        href="https://www.linkedin.com/"
         target="_blank"
         rel="noopener noreferrer"
       >
